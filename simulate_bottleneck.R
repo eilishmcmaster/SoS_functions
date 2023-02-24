9bf3e4076643d1a6e7022f94a47ac5734162573a
eilishmcmaster <eilish.summer@gmail.com>
2023-02-24 14:05:38 +1100

    added example in comments

--- simulate_bottleneck.R
+++ simulate_bottleneck.R
@@ -148,3 +148,40 @@ get_generation_df <- function(out_list,H) {
   
   return(df2)
 }
+
+
+# Example of how to use ####################################
+# 
+# devtools::source_url("https://github.com/eilishmcmaster/SoS_functions/blob/main/simulate_bottleneck.R?raw=TRUE")
+# 
+# num_loci <- 100
+# He_specified <- 0.2
+# Ho_specified <- 0.2
+# n <- 10
+# 
+# generations <- 20
+# pop_size <- rep(c(5,10),15) # more reps means a smoother outcome -- final data is mean 
+# outcross <- c(1,0.5,0) # outcross rates (0=selfing only, 1=non-self only)
+# 
+# out_list <- simulate_populations(pop_size, outcross, generations, num_loci, He_specified, Ho_specified)
+# 
+# 
+# He_df <- get_generation_df(out_list, 'He')
+# Ho_df <- get_generation_df(out_list, 'Ho')
+# 
+# 
+# ggplot(Ho_df, aes(x=generations, y=Ho, groups=factor(popsize_outcrossrate),color=factor(outcross)))+
+#   geom_point()+theme_bw()+geom_line()+
+#   geom_hline(yintercept = c(0.062, 0.038), linetype="dotted")+
+#   # ylim(0)+
+#   geom_vline(xintercept = 0, color="red")+
+#   facet_wrap(popsize~., drop=FALSE)
+# 
+# 
+# ggplot(He_df, aes(x=generations, y=He, groups=factor(popsize_outcrossrate),color=factor(outcross)))+
+#   geom_point()+theme_bw()+geom_line()+
+#   geom_hline(yintercept = c(0.062, 0.038), linetype="dotted")+
+#   # ylim(0)+
+#   geom_vline(xintercept = 0, color="red")+
+#   facet_wrap(popsize~., drop=FALSE)
+# 
