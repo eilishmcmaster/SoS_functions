c864332fd58e4507c0195a4b3d24c216275ceabf
eilishmcmaster <eilish.summer@gmail.com>
2024-03-20 11:30:43 +1100

    add population.pw.fst

--- sos_functions.R
+++ sos_functions.R
@@ -1667,3 +1667,45 @@ infering_function_kin_only <- function(df1, inference_parameters) {
   
   return(df1)
 }
+
+population.pw.Fst <- function (dart_data, population, basedir, species, dataset, 
+                               maf_val = 0.2, miss_val = 0.2, method="W&H02") 
+{
+  # only adds methods option for the user to specify
+  meta <- dart_data$meta
+  p <- population
+  pop_info <- get.population.info(meta, p, method = "average")
+  require(SNPRelate)
+  gds_file <- dart2gds(dart_data, basedir, species, dataset)
+  gds <- snpgdsOpen(gds_file)
+  Fst <- mat.or.vec(pop_info$number, pop_info$number)
+  Nloc <- mat.or.vec(pop_info$number, pop_info$number)
+  Nind <- mat.or.vec(pop_info$number, pop_info$number)
+  for (i in 1:pop_info$number) {
+    for (j in 1:pop_info$number) {
+      if (i > j) {
+        i_pop_indices <- which(p == pop_info$names[i])
+        j_pop_indices <- which(p == pop_info$names[j])
+        ij_pop_indices <- union(i_pop_indices, j_pop_indices)
+        fst <- snpgdsFst(gds, population = as.factor(p[ij_pop_indices]), 
+                         method = method, sample.id = dart_data$sample_names[ij_pop_indices], 
+                         maf = maf_val, missing.rate = miss_val, with.id = TRUE)
+        Fst[i, j] <- fst$Fst
+        Fst[j, i] <- fst$Fst
+        Nloc[i, j] <- length(fst$snp.id)
+        Nloc[j, i] <- length(fst$snp.id)
+        Nind[i, j] <- length(fst$sample.id)
+        Nind[j, i] <- length(fst$sample.id)
+      }
+    }
+  }
+  snpgdsClose(gds)
+  colnames(Fst) <- pop_info$names
+  rownames(Fst) <- pop_info$names
+  colnames(Nloc) <- pop_info$names
+  rownames(Nloc) <- pop_info$names
+  colnames(Nind) <- pop_info$names
+  rownames(Nind) <- pop_info$names
+  flist <- list(Fst = Fst, Nloc = Nloc, Nind = Nind, pop_info = pop_info)
+  return(flist)
+}
