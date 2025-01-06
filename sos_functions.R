a2d184191b6ffa194fd2004c1f0bcac1511f2948
eilishmcmaster <eilish.summer@gmail.com>
2025-01-07 09:43:34 +1100

    allele counts with maf range

--- sos_functions.R
+++ sos_functions.R
@@ -1826,3 +1826,71 @@ faster_allele_counts <- function(dms, pops, min_af) {
   out_df <- out_df[,c(4,1,2,3)]
   return(out_df)
 }
+
+
+
+faster_allele_counts2 <- function(dms, pops, min_af_range) {
+  # Parse the range from the input string
+  min_af <- as.numeric(min(min_af_range))  # Extract lower bound
+  max_af <- as.numeric(max(min_af_range))  # Extract upper bound
+  
+  if (is.na(min_af) || is.na(max_af)) {
+    stop("min_af_range must be a string with two numeric values separated by a comma, e.g., '0.1,0.3'")
+  }
+  
+  groups <- unique({{pops}})
+  out <- vector("list", length(groups))  # Initialize output as a list with the same length as groups
+  
+  ds <- dms$gt
+  keepers <- get_minor_allele_frequencies(ds)
+  
+  # Keep sites with minor allele frequencies within the range
+  ds <- ds[, which(keepers >= min_af & keepers <= max_af)]
+  cat("Found ", ncol(ds), " poly sites within the range [", min_af, ",", max_af, "]\n")
+  
+  ds_inverse <- 2 - ds
+  colnames(ds) <- paste(dms$locus_names[which(keepers >= min_af & keepers <= max_af)], 
+                        substr(dms$locus_nuc[which(keepers >= min_af & keepers <= max_af)], 1, 1), sep="_")
+  colnames(ds_inverse) <- paste(dms$locus_names[which(keepers >= min_af & keepers <= max_af)], 
+                                substr(dms$locus_nuc[which(keepers >= min_af & keepers <= max_af)], 3, 3), sep="_")
+  x <- cbind(ds, ds_inverse)
+  
+  x2 <- x %>% as.matrix()
+  x2[x2==2] <- 1
+  n <- c()
+  out <- list()
+  
+  for (i in seq_along(groups)) {
+    # ds[{{pops}} %in% groups[i], , drop = FALSE]
+    x3 <- x2[{{pops}} %in% groups[i], , drop = FALSE]
+    cat("There are ", nrow(x3), " samples in ", groups[i], "\n")
+    # x3 <- rbind("names" = colnames(x3), as.data.frame(x3))
+    
+    n <- c(n, nrow(x3))
+    allele_max <- colSums(x3, na.rm=TRUE) %>% as.vector()
+    allele_max[allele_max>=1] <- 1 # presence absence
+    out[[i]] <- as.vector(allele_max)
+    names(out)[i] <- groups[i]
+  }
+  
+  z <- do.call(rbind,out)
+  total_allele_count <- rowSums(z, na.rm=TRUE)
+  pa_loci <- which(colSums(z, na.rm=TRUE)==1)
+  
+  if(length(pa_loci)>1){
+    private_allele_count <- rowSums(z[,pa_loci], na.rm=TRUE)
+  }
+  if(length(pa_loci)==1){
+    private_allele_count <- rep(0,nrow(z))
+    private_allele_count[which(z[,pa_loci]==1)] <- 1
+  }
+  if(length(pa_loci)==0){
+    private_allele_count <- rep(0,nrow(z))
+  }
+  out_df <- cbind(private_allele_count, total_allele_count, n) %>% as.data.frame()
+  out_df$population <- rownames(out_df)
+  rownames(out_df) <- NULL
+  out_df <- rbind(out_df, c(ncol(x2), ncol(x2),sum(n), 'Total'))
+  out_df <- out_df[,c(4,1,2,3)]
+  return(out_df)
+}
